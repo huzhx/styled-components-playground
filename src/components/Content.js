50204662907aabca9e084a5d41eeae787c1a8c54
Zhaoxian Hu <huzhx@uci.edu>
2021-05-16 13:08:16 -0700

    :lipstick: implement style components

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #aac9f0;
+`;
+
+const Section = styled.div`
+  background-color: #f0d1aa;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: #f0aeaa;
+  border-radius: 1em;
+`;
+
+const Button = styled.button`
+  background-color: #ffffff;
+  display: flex;
+  justify-content: center;
+  padding: 0.2em;
+  margin: 0.2em;
+  border-radius: 1em;
+`;
 
 const Content = () => {
   return (
-    <div>
-      <h1>Section</h1>
-    </div>
+    <Section>
+      <Title>Section</Title>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </Section>
   );
 };
 
